bc48ad88ac1d1ba7a4c3dcb2428f4ed24e9b3138
Humphrey <h.nyahoja@alustudent.com>
2023-07-10 23:31:28 -0700

    kdk changed

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
+#humphrey
+if __name__ == "__main__":
     import hidden_4
     import sys
 
     for n in dir(hidden_4):
         if n[:2] != "__":
             print(n)
-
-
-if __name__ == "__main__":
