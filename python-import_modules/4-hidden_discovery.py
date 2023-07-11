c4274f0d6822667c58a9752a96a7a5780f2d210a
Humphrey <h.nyahoja@alustudent.com>
2023-07-10 23:15:26 -0700

    hgfh changed

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 
-import imp
+    import hidden_4
+    import sys
 
-def print_hidden_names(module_path):
-  module = imp.load_compiled("hidden_4", module_path)
-  names = [name for name in dir(module) if not name.startswith("__")]
-  names.sort()
-  for name in names:
-    print(name)
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
+
 
 if __name__ == "__main__":
-  print_hidden_names("hidden_4.pyc")
