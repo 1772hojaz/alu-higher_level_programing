18faf86e4ccec44a09dc654f52ab4c179e7641b3
Humphrey <h.nyahoja@alustudent.com>
2023-07-10 17:10:59 -0700

    jona

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,8 +3,11 @@ def add(a, b):
     value1 = a
     value2 = b
     sum = value1 + value2
-    print(f"{value1} + {value2} = {sum}")
+    print("{} + {} = {}".format(value1, value2, sum))
     return(sum)
-add(1,2)
+
+def main():
+    add(1,2)
+
 if __name__ == "__main__":
     main()
