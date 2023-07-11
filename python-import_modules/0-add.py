ffa9a3484d665db333e9b1fb018c7df74cffc2c1
Humphrey <h.nyahoja@alustudent.com>
2023-07-10 18:50:43 -0700

    YOU changed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,8 +7,9 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    sum = add (a, b)
+    sum = add(a, b)
     print("{} + {} = {}".format(a, b, sum))
+
 
 if __name__ == "__main__":
     main()
