72c7f586b357896822f1062bd704308af2c69633
Humphrey <h.nyahoja@alustudent.com>
2023-07-10 22:22:48 -0700

    ook0o changed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,14 +3,16 @@
 
 import sys
 
+
 def main():
     args = sys.argv[1:]
     num_args = len(args)
-    print("{} argument{}.".format(num_args, "s" if num_args != 1 else ""))
-    if num_args > 0:
+    if num_args == 1:
+        print("1 argument:")
+    else:
         print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""))
-        for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
 
 
 if __name__ == "__main__":
