eaba9cd1a07166a1d857785049b62f549d855f41
Humphrey <h.nyahoja@alustudent.com>
2023-07-10 18:00:17 -0700

    the commit from humphrey

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-import add_0
+import add_0 as addition
 
 def main():
     a = 1
     b = 2
-    sum = add_0.add(a, b)
+    sum = addition.add(a, b)
     print("{} + {} = {}".format(a, b, sum))
 
 if __name__ == "__main__":
