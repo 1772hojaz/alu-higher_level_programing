0fca4be09fcf886d91c1494e1fff888a3dea661d
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 14:43:30 -0700

    jj changed

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,13 @@
 element_at = __import__('1-element_at').element_at
 
 
-my_list = [1, 2, 3, 4, 5]
-def main ():
+def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
+def main():
+    element_at(my_list, idx)
+
+
+if __name__ == "__main__":
+    main()
