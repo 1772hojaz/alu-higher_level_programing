682ac660233e445626e0c221d6ccd45f0eda4430
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 15:16:30 -0700

    ffy changed

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or >= len(my_list):
+        return my_list
+def main():
+    replace_in_list(my_list, idx, element)
+
+
+if __name__ == "__main__":
+    main()
