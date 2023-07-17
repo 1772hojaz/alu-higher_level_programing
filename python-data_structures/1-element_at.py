6c1633de03e34261365bdb4cff632b2913f45da2
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 13:27:58 -0700

    1-element_at.py is corrected changed

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
 
 
 my_list = [1, 2, 3, 4, 5]
 def main ():
-    for idx, elem in enumerate(my_list):
-        print("Element at index {:d} is {}".format(idx, elem))
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    return my_list[idx]
 
 
 if __name__ == "__main__":
