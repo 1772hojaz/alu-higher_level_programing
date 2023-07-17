b7c985021a3b16392dd975211ae9389290360a4c
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 15:48:59 -0700

    bb changed

---
+++ python-data_structures/3-print_reversed_list_inteder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
+
+
+def main ():
+    print_reversed_list_integer(my_list=[])
+
+
+if __name__ == "__main__":
+    main()
