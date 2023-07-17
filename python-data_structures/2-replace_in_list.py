b3972505fbc3d7690a0fc927c4a1c7bbc97d8840
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 15:25:01 -0700

    hoobk changed

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,9 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or >= len(my_list):
         return my_list
+    my_list[idx] = element
+
+
 def main():
     replace_in_list(my_list, idx, element)
 
