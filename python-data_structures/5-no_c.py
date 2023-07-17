fc2dbe379c67fa4d3d20a1c3c7311c9e5d82d92f
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 16:16:17 -0700

    kk changed

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    new_string = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
+def main():
+    no_c(my_string)
+
+
+if __name__ == "__main__":
+    main()
