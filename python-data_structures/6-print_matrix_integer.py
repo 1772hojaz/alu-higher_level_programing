67330ec380d91c848b06ea847cb00d4d6c3e2c92
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 16:24:31 -0700

    jj changed

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i != len(row) - 1:
+                print(" ", end="")
+
+
+def new_line():
+    print()
+
+
+def main():
+    print_matrix_integer(matrix=[[]]))
+    new_line
+
+
+if __name__ == "__main__":
+    main()
