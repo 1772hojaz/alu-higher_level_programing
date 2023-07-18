970c934bfe26a88783ac163ee491e5e3daaed816
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 19:38:23 -0700

    siya

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def only_diff_elements(set_1, set_2):
+    diff_set = set_1.symmetric_difference(set_2)
+    return diff_set
+
+
+def main():
+    only_diff_elements(set_1, set_2)
+
+
+if __name__ == "__main__":
+    main()
