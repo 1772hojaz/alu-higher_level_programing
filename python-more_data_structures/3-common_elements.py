61ee49acbe024e814ff9efe6352e994ceb76379d
Humphrey <h.nyahoja@alustudent.com>
2023-07-17 19:28:36 -0700

    joo

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common_set = set_1.intersection(set_2)
+    return common_set
+
+def main():
+    common_elements(set_1, set_2)
+
+
+if __name__ == "__main__":
+    main()
