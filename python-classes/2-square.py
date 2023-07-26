25ea0ad92005f4d008a58daf6608342223c7e52e
Humphrey <h.nyahoja@alustudent.com>
2023-07-26 02:54:19 -0700

    hgfd

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ its about  asquare"""
 class Square:
     """This is the class"""
     def __init__(self, size=0):
-        if type(size_new) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
 
         elif size_new < 0:
