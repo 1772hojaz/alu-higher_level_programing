e90820751fcc40cda0e7a10f0c8acbc4753462c0
Humphrey <h.nyahoja@alustudent.com>
2023-07-26 05:46:43 -0700

    youuu

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """area calculation"""
+
+
 class Square:
     """we square the side of a square to get its area"""
     def __init__(self, size=0):
         self.__size = size
+
     def area(self):
         return self.__size**2
-
