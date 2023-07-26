49ec8efa4b6d3110106963c47feb6cfc124dbbb2
Humphrey <h.nyahoja@alustudent.com>
2023-07-26 16:19:45 -0700

    hhd

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,36 +3,27 @@
 
 
 class Square:
-    """creating a square
-    private instance attribute"""
-    def __init__(self, size=0):
+    """Creates a square.
+    Private instance attributes"""
+
+    def _init_(self, size=0):
+        """Initialisation."""
         self.__size = size
 
-
     @property
-    """this is to get the private value
-    of size a private attribute. we use getter method"""
     def size(self):
-        return self.__size = size
-
+        """Retrieve size."""
+        return self.__size
 
     @size.setter
-    """this is to make sure that the value that
-    we got with the getter can be used and or modified """
     def size(self, value):
-        """to set it:
-        size must be an integer, otherwis
-        e raise a TypeError exception with
-        the message size must be an integer"""
-        if type(size) is not int:
+        """Sets size to a value."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        """if size is less than 0, raise a ValueError
-        exception with the message size must be >= 0"""
         elif value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
-        """Public instance method: def area(self):
-            that returns the current square area"""
     def area(self):
-        return self.__size**2
+        """Returns square area."""
+        return self.__size ** 2
