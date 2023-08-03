8823d9b1c36f9d2ff4311fe3004e06a6ec0ae866
Humphrey <h.nyahoja@alustudent.com>
2023-08-03 15:00:04 -0700

    adding all files

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- write a script that computes the score average of all records in the table
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Write a script that computes the score average of all records in 
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL ORDER BY score DESC;
