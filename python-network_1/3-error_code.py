60b25c8fc378623bafa58054cf6c6c231ad3ec23
humphrey <h.nyahoja@alustudent.com>
2023-08-04 13:04:42 +0000

    hoyo

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,6 +1,5 @@
 cat > 3-	
 #!/usr/bin/python3
-# the python script
 """
     send a request to the URL & display body of the response
 """
@@ -13,8 +12,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
-	"""send request"""
-    try:
+	"""send the request"""
+   try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
