1018afcf6e9a6d5558cb79c1b798bbc51f6c823b
Humphrey <h.nyahoja@alustudent.com>
2023-08-03 18:03:15 -0700

    all files

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of MySQL users on my localhost
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
