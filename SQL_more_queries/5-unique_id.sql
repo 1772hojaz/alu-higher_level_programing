6b68b4a59b4011ed8fd9bd5de92d797346d861d3
Humphrey <h.nyahoja@alustudent.com>
2023-08-04 06:50:05 -0700

    file 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,3 @@
 -- script that creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+
