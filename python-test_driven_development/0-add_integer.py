6bf8bb43d69e717983fbd55c24a542993e2bf277
Humphrey <h.nyahoja@alustudent.com>
2023-08-15 04:06:17 -0700

    the first file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''this funtion adds 2 integers'''
 
+
 def add_integer(a, b=98):
     '''a funtion that add two integers 
     it has 2 arguments
     '''
-    if type(a) == float or type(b)==float:
+    if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
     if type(a) != int:
