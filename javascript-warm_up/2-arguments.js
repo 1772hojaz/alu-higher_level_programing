a4923fe561874c0bb91bfd4c712eaa89fa38e721
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2023-09-30 07:06:34 -0700

    ok

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 const numofArg = process.arg.leng - 2;
-if (numofArg === 0){
+if (numofArg === 0)
+{
 	console.log('No argument');
-}else if (numofArg === 1) {
+}
+else if (numofArg === 1) 
+{
 	console.log('Argument found');
-}else {
+}
+else 
+{
 	console.log('Arguments found');
 }
