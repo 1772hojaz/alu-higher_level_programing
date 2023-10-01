bdcbabc97a643370671f64d87f40cb8438c0925d
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2023-09-30 18:57:59 -0700

    all files

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (isNaN(num) || num < 0) {
+    return 1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
