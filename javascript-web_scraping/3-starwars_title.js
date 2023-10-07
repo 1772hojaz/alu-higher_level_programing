845afeddbd892d39b5936674114060ca500abd36
Humphrey <h.nyahoja@alustudent.com>
2023-10-07 20:26:17 +0000

    added file 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//importing the request module
+const req = require('request');
+
+//taking input from the command line
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+req.get(url, (err, res) => {
+  err ? console.log(err) : console.log(JSON.parse(res.body).title);
+});
