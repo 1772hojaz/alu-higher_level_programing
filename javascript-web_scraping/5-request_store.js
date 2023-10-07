4277912917b17bfb50c61b81db025557d5b17427
Humphrey <h.nyahoja@alustudent.com>
2023-10-07 20:57:21 +0000

    added file 5 and 6

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// using a request module
+const req = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+req.get(url, async (err, res) => {
+  err
+    ? console.log(err)
+    : await fs.writeFile(file, res.body, (err) => {
+      if (err) console.log(err);
+    });
+});
